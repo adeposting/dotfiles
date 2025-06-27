1abc9e8a0e076571fdda195dffb80520fe16d53b
adeposting <217110652+adeposting@users.noreply.github.com>
2025-06-27 06:14:06 -0700

    Add shell aliases configuration

---
+++ src/.profile.d/aliasrc.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# --- Navigation ---
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias -- -='cd -'
+
+# --- Overrides ---
+alias mkdir='mkdir -pv'
+alias grep='grep --color=auto'
+alias top='htop'
+alias ping='ping -c 3'
+alias wget='wget -c'
+alias tree='tree -C'
+
+# --- Unix Shortcuts ---
+alias ls='ls -GFh'
+alias ll='ls -lh'
+alias la='ls -lha'
+alias l='ls -lhA'
+
+# --- Terminal Shortcuts ---
+alias c='clear'
+alias du1='du -h -d 1'
+
+# --- Editor Shortcuts ---
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+
+# --- Application Shortcuts ---
+alias vscode='open -a "Visual Studio Code"'
