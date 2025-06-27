91c83365211b2429bf260159ebda35b721da7bad
adeposting <217110652+adeposting@users.noreply.github.com>
2025-06-27 06:14:06 -0700

    Add environment variables configuration

---
+++ src/.profile.d/envrc.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LESS='-R'
+export HISTSIZE=10000
+export SAVEHIST=10000
+export DIRSTACKSIZE=10
+
+for d in ~/env ~/.env ~/.env.d ~/.local/env ~/.local/.env; do
+    if [[ -f "$d" ]]; then
+        source "$d"
+    elif [[ -d "$d" ]]; then
+        for f in "$d"/*; do
+            [[ -f "$f" ]] && source "$f"
+        done
+    fi
+done
+
+if [[ -x /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
